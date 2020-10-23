3ff40520c27a61476c809178e90d2fb3e901bcf4
HueVu <huehuong982@gmail.com>
2020-10-23 13:00:22 +0700

    huevu

--- src/FormMain.java
+++ src/FormMain.java
@@ -52,7 +52,7 @@ public class FormMain extends JFrame {
 	/**
 	 * Declare JObjects on JFrame
 	 */
-	//tes1
+	//tes123
 	private JPanel contentPane;
 	private JLabel lbTitle;
 	private JPanel pnImitiate;
